a198cd06acb74624b06476b84b75057638ceac2e
blank_popup <ha6446@gmail.com>
2025-11-02 08:50:34 +0900

    feat: Spring MVC function
    Renaming
    ReqRes.java -> MvcUtil.java
    
    Adding method
    - getCurrentUserDetails
    - getCurrentUsername
    - getCurrentToken

---
+++ src/main/java/org/duckdns/ahamike/rollbook/util/springmvc/MvcUItil.java
@@ -0,0 +1,61 @@
+package org.duckdns.ahamike.rollbook.util.springmvc;
+
+import org.duckdns.ahamike.rollbook.config.security.UserDetailsCustom;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.StringUtils;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class MvcUItil {
+    public static HttpServletRequest getCurrentRequest() {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (requestAttributes == null) {
+            return null;
+        }
+
+        if (requestAttributes instanceof ServletRequestAttributes servletRequestAttributes) {
+            return servletRequestAttributes.getRequest();
+        }
+        return null;
+    }
+
+    public static UserDetails getCurrentUserDetails() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = auth.getPrincipal();
+        if (principal instanceof UserDetailsCustom == false) {
+            return null;
+        }
+        UserDetails userDetails = (UserDetails) principal;
+
+        return userDetails;
+    }
+
+    public static String getCurrentUsername() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.isAuthenticated() == true && "anonymousUser".equals(auth.getPrincipal()) == false) {
+            return auth.getName();
+        }
+
+        return null;
+    }
+
+    public static String getCurrentToken() {
+        HttpServletRequest request = getCurrentRequest();
+        if (request == null) {
+            return null;
+        }
+
+        String authorization = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(authorization) && authorization.startsWith("Bearer ")) {
+            return authorization.substring(7);
+        }
+
+        return null;
+    }
+}
