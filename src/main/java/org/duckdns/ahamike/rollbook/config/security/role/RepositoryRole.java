b66b7fb67d367a812272347bc202ffe14a33486d
blank_popup <ha6446@gmail.com>
2025-11-20 15:37:06 +0900

    feat: Tenant
    Configuration of tenant
    Managing tenant
    - Adding and removing tenant
    - Getting tenant list
    
    To do
    - Cache for tenant
    - more fast to read information of tenant

--- src/main/java/org/duckdns/ahamike/rollbook/config/security/role/RepositoryRole.java
+++ src/main/java/org/duckdns/ahamike/rollbook/config/security/role/RepositoryRole.java
@@ -1,6 +1,7 @@
 package org.duckdns.ahamike.rollbook.config.security.role;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import org.duckdns.ahamike.rollbook.table.EntityRole;
@@ -9,4 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface RepositoryRole extends JpaRepository<EntityRole, Long> {
     Set<EntityRole> findAllByNameIn(List<String> roles);
     boolean existsByName(String name);
+    Optional<EntityRole> findByName(String roleName);
 }
