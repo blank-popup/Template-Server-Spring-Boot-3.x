6868eac608ce4d6f16bd6d49c200ca698a1a9165
blank_popup <ha6446@gmail.com>
2025-11-27 09:25:28 +0900

    fix: required a single bean, but 2 were found
    Parameter 2 of constructor in org.duckdns.ahamike.rollbook.config.security.ConfigSecurityTest required a single bean, but 2 were found:
            - corsConfigurationSource: defined by method 'corsConfigurationSource' in class path resource [org/duckdns/ahamike/rollbook/config/WebConfig.class]
            - mvcHandlerMappingIntrospector: defined by method 'mvcHandlerMappingIntrospector' in class path resource [org/springframework/boot/autoconfigure/web/servlet/WebMvcAutoConfiguration$EnableWebMvcConfiguration.class]
    
    This may be due to missing parameter name information
    
    Problem: Two beans existed
    - Bean I create using @Bean
    - Bean created by implements WebMvcConfigurer
    
    Solution
    Override bean from implements WebMvcConfigurer

--- src/main/java/org/duckdns/ahamike/rollbook/config/WebConfig.java
+++ src/main/java/org/duckdns/ahamike/rollbook/config/WebConfig.java
@@ -8,9 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -34,16 +32,12 @@ public class WebConfig implements WebMvcConfigurer {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOriginPattern("*");
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
-        return urlBasedCorsConfigurationSource;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
